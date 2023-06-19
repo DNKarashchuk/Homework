29366b66b2502753d79ae4ee8d151d0771b890d2
Dmitriy <DNKarashcuk@gmail.com>
2023-06-19 19:28:02 +0300

    one seminar

---
+++ task0/Program.cs
@@ -0,0 +1,11 @@
+﻿// // Написать программу
+// Console.WriteLine("Hello, World!");
+ 
+//  4 -> 16;
+//  -3 -> 9;
+//  -7 -> 49;
+
+Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int quater = number * number;
+Console.WriteLine($"Квадрат числа {number} равен {quater}");
