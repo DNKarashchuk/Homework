2d1cf8f40c22612959a52794649eb801dc9f5c22
Dmitriy <DNKarashcuk@gmail.com>
2023-06-20 01:28:36 +0300

    OneSeminar

--- homework 4/Program.cs
+++ homework 4/Program.cs
@@ -1,6 +1,22 @@
-﻿// // See https://aka.ms/new-console-template for more information
-// Console.WriteLine("Hello, World!");
-// Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
-// 2, 3, 7 -> 7
-// 44 5 78 -> 78
-// 22 3 9 -> 22
+﻿// // Задача 4: Напишите программу, которая принимает на вход
+//  три числа и выдаёт максимальное из этих чисел.
+// // 2, 3, 7 -> 7
+// // 44 5 78 -> 78
+// // 22 3 9 -> 22
+
+Console.Write("Введите число: ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число: ");
+int numberB = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число: ");
+int numberC = Convert.ToInt32(Console.ReadLine());
+
+int max = numberA;
+
+if (max < numberB) max = numberB;
+if (max < numberC) max = numberC;
+
+System.Console.WriteLine("Максимальное из этих чисел: " + max);
+
+// System.Console.Write("max = ");
+// System.Console.WriteLine(max);
--- homework 6/Program.cs
+++ homework 6/Program.cs
@@ -1,6 +1,18 @@
-﻿// // See https://aka.ms/new-console-template for more information
-// Console.WriteLine("Hello, World!");
-// Задача 6: Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
-// 4 -> да
-// -3 -> нет
-// 7 -> нет
+﻿// // Задача 6: Напишите программу, которая на вход принимает число и выдаёт, 
+// является ли число чётным (делится ли оно на два без остатка).
+// // 4 -> да
+// // -3 -> нет
+// // 7 -> нет
+
+
+Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if ( number % 2 == 0 )
+{
+System.Console.WriteLine($"Число: {number} четное ");
+}
+else
+{
+System.Console.WriteLine($"Число: {number} нечетное");
+}
